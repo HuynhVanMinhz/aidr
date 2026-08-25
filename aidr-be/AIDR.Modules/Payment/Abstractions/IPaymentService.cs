1b5304ccbb6553fab773d52c1342a3444eeaa626
Ha20203 <tranthiphuongha20022003@gmail.com>
2026-08-25 10:40:43 +0700

    implement payment functionality with PayOS integration

---
+++ aidr-be/AIDR.Modules/Payment/Abstractions/IPaymentService.cs
@@ -0,0 +1,15 @@
+using AIDR.Shared.Dtos.Payment;
+
+namespace AIDR.Modules.Payment.Abstractions;
+
+public interface IPaymentService
+{
+    Task<CreatePayOsPaymentResponse> CreatePayOsPaymentAsync(
+        Guid buyerUserId,
+        CreatePayOsPaymentRequest request,
+        CancellationToken cancellationToken = default);
+
+    Task<PayOsWebhookResult> HandlePayOsWebhookAsync(
+        PayOsWebhookRequest request,
+        CancellationToken cancellationToken = default);
+}
